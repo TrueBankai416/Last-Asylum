116947a5dade81f697b82c906d8fa66642b5a1cc
TrueBankai416 <carrigan@lunekes.com>
2026-04-15 15:42:41 -0400

    chore: update production URL to custom domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://truebankai416.github.io',
+  url: 'https://last-asylum.bankai-tech.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
